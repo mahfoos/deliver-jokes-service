bb1566ae7073821d57bb085df64f364b6f710665
Mahfoos <umahfoosahamed@gmail.com>
2024-11-02 16:07:43 +0530

    updated config

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -4,6 +4,6 @@ export default registerAs('database', () => ({
   host: process.env.DB_HOST || 'localhost',
   port: parseInt(process.env.DB_PORT, 10) || 3306,
   username: process.env.DB_USERNAME || 'root',
-  password: process.env.DB_PASSWORD || 'root',
+  password: process.env.DB_PASSWORD || 'root12345',
   database: process.env.DB_DATABASE || 'jokes_db',
 }));
