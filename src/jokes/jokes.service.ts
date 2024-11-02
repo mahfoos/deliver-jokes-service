74b1535f9d5f795a3f0732b86c22e3ac75b2d5d5
Mahfoos <umahfoosahamed@gmail.com>
2024-11-02 21:13:22 +0530

    updated the logic

--- src/jokes/jokes.service.ts
+++ src/jokes/jokes.service.ts
@@ -2,7 +2,6 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Joke } from '../entities/joke.entity';
-import { CreateJokeDto } from '../dto/create-joke.dto';
 
 @Injectable()
 export class JokesService {
@@ -10,21 +9,6 @@ export class JokesService {
     @InjectRepository(Joke)
     private jokesRepository: Repository<Joke>,
   ) {}
-
-  async create(createJokeDto: CreateJokeDto): Promise<Joke> {
-    const joke = this.jokesRepository.create(createJokeDto);
-    return await this.jokesRepository.save(joke);
-  }
-
-  async findAll(): Promise<Joke[]> {
-    return await this.jokesRepository.find({ where: { isActive: true } });
-  }
-
-  async findByType(type: string): Promise<Joke[]> {
-    return await this.jokesRepository.find({
-      where: { type, isActive: true },
-    });
-  }
 
   async getRandomJoke(type?: string): Promise<Joke> {
     const query = this.jokesRepository
