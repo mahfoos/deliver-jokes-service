adb3b14eaeefd5973cc09f7f76e5e227349e8e98
Mahfoos <umahfoosahamed@gmail.com>
2024-11-03 14:18:12 +0530

    updated config

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,9 +1,9 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('database', () => ({
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT, 10) || 3306,
-  username: process.env.DB_USERNAME || 'root',
-  password: process.env.DB_PASSWORD || 'root12345',
-  database: process.env.DB_DATABASE || 'jokes_db',
+  host: process.env.DB_HOS,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 }));
